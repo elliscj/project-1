e2c48a68a28f11b8407ce47943bb1eaee69a2156
djmarcy <dj_marcy@hotmail.com>
2021-11-16 17:15:17 -0700

    JavaScript framework

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Variables
+let searchButton = document.getElementById("")
+let interactiveMap = document.getElementById("")
+let infoCards = document.getElementsByClassName("")
+let previousSearches = document.getElementsByClassName("")
+
+// API functionality
+
+
+//Functions
+function search() {}
+
+function getBarInfo () {}
+
+function recordSearches() {}
+
+function clearSearches() {}
+
+//Event Listeners
