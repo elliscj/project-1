cb9d2e805bfe00e6aa99263c7e889a10a613f947
Cam Ellis <elliscj@colorado.edu>
2021-11-17 22:47:16 -0700

    cmnd save, prettier just formatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,18 @@
 var requestUrl =
   "https://www.mapquestapi.com/geocoding/v1/address?key=1zplhBsQEJyaGd98SGJ6HjcN66lVvSDL";
 
-let searchLocation = ""// prompt("Where would you like to drink beer?");
-let beerLocation = ""
+let searchLocation = ""; // prompt("Where would you like to drink beer?");
+let beerLocation = "";
 
 console.log(searchLocation);
 console.log(beerLocation);
 
-// window.onload = 
+// window.onload =
 function findBreweries() {
-
-  searchLocation = document.getElementById("cityreq").value 
+  searchLocation = document.getElementById("cityreq").value;
   beerLocation = searchLocation;
-  console.log(beerLocation)
-  
+  console.log(beerLocation);
+
   getApi();
 
   L.mapquest.key = "1zplhBsQEJyaGd98SGJ6HjcN66lVvSDL";
@@ -32,21 +31,24 @@ function findBreweries() {
       layers: L.mapquest.tileLayer("map"),
       zoom: 14,
     });
-}};
+  }
+}
 
-document.getElementById("submit").addEventListener("click", findBreweries)
+document.getElementById("submit").addEventListener("click", findBreweries);
 
 // var place = document.getElementById("#place-search-input");
 
-console.log("./imgs/img-1.jpg")
+console.log("./imgs/img-1.jpg");
 
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
 
   brewUrl =
-  "https://api.openbrewerydb.org/breweries?by_city=" + beerLocation + "&per_page=4"
+    "https://api.openbrewerydb.org/breweries?by_city=" +
+    beerLocation +
+    "&per_page=4";
 
-console.log(brewUrl);
+  console.log(brewUrl);
 
   //fetch returns us a promise object - promise obj have 3 states - pending resolved rejected
   fetch(brewUrl)
@@ -59,17 +61,17 @@ console.log(brewUrl);
       console.log(data[0].street, data[0].city, ",", data[0].state);
       console.log(data[0].name);
 
-    for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < 4; i++) {
+        let backgroundImages = Math.floor(Math.random() * 19) + 1;
 
-        let backgroundImages = Math.floor(Math.random() * 19) + 1
-
-        document.getElementsByClassName("card-title")[i].textContent = data[i].name;
-        // document.getElementsByClassName("card-imgs")[i].src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&psig=AOvVaw17UayflL5vNx0VPaxoqbDO&ust=1637295349984000&source=images&cd=vfe&ved=2ahUKEwicl4zPhqH0AhWCK6wKHQhNA6AQr4kDegUIARDMAQ" // "./imgs/img-" + backgroundImages + ".jpg"; 
-        document.getElementsByClassName("card-text")[i].textContent = data[i].street, data[i].city, data[i].state;
+        document.getElementsByClassName("card-title")[i].textContent =
+          data[i].name;
+        // document.getElementsByClassName("card-imgs")[i].src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&psig=AOvVaw17UayflL5vNx0VPaxoqbDO&ust=1637295349984000&source=images&cd=vfe&ved=2ahUKEwicl4zPhqH0AhWCK6wKHQhNA6AQr4kDegUIARDMAQ" // "./imgs/img-" + backgroundImages + ".jpg";
+        (document.getElementsByClassName("card-text")[i].textContent =
+          data[i].street),
+          data[i].city,
+          data[i].state;
         document.getElementById("card-url").textContent = data[i].website_url;
-
-    }
-
-
+      }
     });
 }
